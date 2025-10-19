d71e4e6374aeaa6d62a7a1a0dd5199e7538a5da6
jumana <j9oo@icloud.com>
2025-10-19 10:34:01 +0300

    Initial Commit

---
+++ plant/plantApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantApp.swift
+//  plant
+//
+//  Created by Jumanah aldowaish on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct plantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
